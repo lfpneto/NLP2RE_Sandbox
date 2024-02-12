730903633abdb7ab4842b35fd86cc34396733dcd
lfneto <luis.neto@criticalsoftware.com>
2024-02-12 11:48:03 +0000

    Refactoring of code

--- App/utils/CleanData.py
+++ App/utils/CleanData.py
@@ -1,8 +1,14 @@
 import string
 import re
 import nltk
+from nltk.stem import WordNetLemmatizer
+
 
 stopwords = nltk.corpus.stopwords.words('english')
+wn = nltk.WordNetLemmatizer()
+ps = nltk.PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
 
 def remove_punct(text):
     text_nopunct = "".join([char for char in text if char not in string.punctuation])
@@ -12,12 +18,13 @@ def tokenize(text):
     tokens = re.split('\W+', text)
     return tokens
 
-wn = nltk.WordNetLemmatizer()
-ps = nltk.PorterStemmer()
-
 def lemmatizing(tokenized_text):
-    text = [wn.lemmatize(word) for word in tokenized_text]
+    # Initialize WordNetLemmatizer
+    # Lemmatize each word in the tokenized text
+    text = [lemmatizer.lemmatize(word) for word in tokenized_text]
+    
     return text
+
 
 def clean_text_original(text):
     text = "".join([word.lower() for word in text if word not in string.punctuation])
@@ -55,9 +62,16 @@ def clean_text(text, remove_punct=True, remove_stopwords=True):
     
     return tokens
 
+def join_list_to_string(attribute):
+    if isinstance(attribute, list):
+        return ' '.join(attribute)
+    else:
+        return str(attribute)
+    
+
 def clean_and_lemm_text(text):
-    text_clean = clean_text(text)
+    text_clean = clean_text(text, False, False)
     text_lemm = lemmatizing(text_clean)
-    list_2_word = ' '.join(text_lemm)
+    list_2_word = join_list_to_string(text_lemm)
     
     return list_2_word
