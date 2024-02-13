f9339863a71165f0392c1d69ed5fd7df96454ba5
lfneto <luis.neto@criticalsoftware.com>
2024-02-13 12:20:12 +0000

    setup "test_clean_data"

---
+++ Tests/utils/test_clean_data.py
@@ -0,0 +1,66 @@
+# test_clean_data.py
+
+from src.utils import clean_data
+import string
+
+
+def test_remove_punct():
+    # Test input with punctuation
+    text_with_punct = "Hello, World! How are you?"
+    expected_result = "Hello World How are you"
+
+    # Call the remove_punct function
+    result = clean_data.remove_punct(text_with_punct)
+
+    # Assert that the result matches the expected output
+    assert result == expected_result
+
+    # Test input with no punctuation
+    text_without_punct = "This is a sentence without any punctuation"
+    expected_result = text_without_punct
+
+    # Call the remove_punct function
+    result = clean_data.remove_punct(text_without_punct)
+
+    # Assert that the result matches the expected output
+    assert result == expected_result
+
+    # Test input with only punctuation
+    text_only_punct = "!@#$%^&*()_+"
+    expected_result = ""
+
+    # Call the remove_punct function
+    result = clean_data.remove_punct(text_only_punct)
+
+    # Assert that the result matches the expected output
+    assert result == expected_result
+
+
+def test_tokenize():
+    # TODO:
+    pass
+
+
+def test_lemmatizing():
+    # TODO:
+    pass
+
+
+def test_clean_text_original():
+    # TODO:
+    pass
+
+
+def test_clean_text():
+    # TODO:
+    pass
+
+
+def test_join_list_to_string():
+    # TODO:
+    pass
+
+
+def test_clean_and_lemm_text():
+    # TODO:
+    pass
--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -11,37 +11,44 @@ lemmatizer = WordNetLemmatizer()
 
 
 def remove_punct(text):
-    text_nopunct = "".join([char for char in text if char not in string.punctuation])
+    # FIXME: the '-' caracter is deleted
+    # FIXME: multiple whitespaces are not deleted
+    text_nopunct = "".join(
+        [char for char in text if char not in string.punctuation])
     return text_nopunct
+
 
 def tokenize(text):
     tokens = re.split('\W+', text)
     return tokens
 
+
 def lemmatizing(tokenized_text):
     # Initialize WordNetLemmatizer
     # Lemmatize each word in the tokenized text
     text = [lemmatizer.lemmatize(word) for word in tokenized_text]
-    
+
     return text
 
 
 def clean_text_original(text):
-    text = "".join([word.lower() for word in text if word not in string.punctuation])
+    text = "".join([word.lower()
+                   for word in text if word not in string.punctuation])
     tokens = re.split('\W+', text)
-    text = " ".join([ps.stem(word) for word in tokens if word not in stopwords])
+    text = " ".join([ps.stem(word)
+                    for word in tokens if word not in stopwords])
     return text
 
 
 def clean_text(text, remove_punct=True, remove_stopwords=True):
     """
     Clean the input text by removing punctuation, stopwords, and performing tokenization.
-    
+
     Parameters:
     - text (str): The input text to be cleaned.
     - remove_punct (bool): Flag to indicate whether to remove punctuation or not. Default is True.
     - remove_stopwords (bool): Flag to indicate whether to remove stopwords or not. Default is True.
-    
+
     Returns:
     - cleaned_text (list of str): The cleaned list of tokens.
     """
@@ -51,27 +58,28 @@ def clean_text(text, remove_punct=True, remove_stopwords=True):
     # Remove punctuation
     if remove_punct:
         text = text.translate(str.maketrans('', '', string.punctuation))
-    
+
     # Tokenize the text using regular expression
     tokens = re.findall(r'\b\w+(?:-\w+)*\b', text.lower())
-    
+
     # Remove stopwords if required
     if remove_stopwords:
         stop_words = set(stopwords.words('english'))
         tokens = [word for word in tokens if word not in stop_words]
-    
+
     return tokens
+
 
 def join_list_to_string(attribute):
     if isinstance(attribute, list):
         return ' '.join(attribute)
     else:
         return str(attribute)
-    
+
 
 def clean_and_lemm_text(text):
     text_clean = clean_text(text, False, False)
     text_lemm = lemmatizing(text_clean)
     list_2_word = join_list_to_string(text_lemm)
-    
+
     return list_2_word
